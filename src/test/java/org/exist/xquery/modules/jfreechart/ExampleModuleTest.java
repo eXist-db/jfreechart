b7cf942e7f7c1fda578f34dbb48b6c224e858225
Dannes Wessels <dannes@exist-db.org>
2019-06-12 20:36:20 +0200

    No tests (yet)

--- src/test/java/org/exist/xquery/modules/jfreechart/ExampleModuleTest.java
+++ src/test/java/org/exist/xquery/modules/jfreechart/ExampleModuleTest.java
@@ -1,17 +1,8 @@
 package org.exist.xquery.modules.jfreechart;
 
 
-import org.exist.EXistException;
-import org.exist.security.PermissionDeniedException;
-import org.exist.storage.BrokerPool;
-import org.exist.storage.DBBroker;
 import org.exist.test.ExistEmbeddedServer;
-import org.exist.xquery.XPathException;
-import org.exist.xquery.XQuery;
-import org.exist.xquery.value.Sequence;
 import org.junit.ClassRule;
-
-import java.util.Optional;
 
 public class ExampleModuleTest {
 
@@ -21,12 +12,12 @@ public class ExampleModuleTest {
 
 
 
-    private Sequence executeQuery(final String xquery) throws EXistException, PermissionDeniedException, XPathException {
-        final BrokerPool pool = existEmbeddedServer.getBrokerPool();
-        final XQuery xqueryService = pool.getXQueryService();
-
-        try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()))) {
-            return xqueryService.execute(broker, xquery, null);
-        }
-    }
+//    private Sequence executeQuery(final String xquery) throws EXistException, PermissionDeniedException, XPathException {
+//        final BrokerPool pool = existEmbeddedServer.getBrokerPool();
+//        final XQuery xqueryService = pool.getXQueryService();
+//
+//        try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()))) {
+//            return xqueryService.execute(broker, xquery, null);
+//        }
+//    }
 }
